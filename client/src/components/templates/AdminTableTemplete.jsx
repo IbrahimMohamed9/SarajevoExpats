1c3dacfff32267acfbaad6d487e8147f45eeaedd
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-12-12 13:06:09 +0100

    create table templete

---
+++ client/src/components/templates/AdminTableTemplete.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import CollapsibleTable from "@organisms/CollapsibleTable";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import axiosInstance from "@/config/axios";
+
+const AdminTableTemplete = async ({ title, tableKey, subDataTitle }) => {
+  const response = await axiosInstance.get(`/${tableKey}`);
+
+  return (
+    <div className="m-auto flex flex-col gap-4 pr-3 pt-4">
+      <div className="flex justify-between">
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Button variant="contained" color="primary">
+          Add New
+        </Button>
+      </div>
+      <CollapsibleTable
+        tableKey={tableKey}
+        data={response.data}
+        subDataTitle={subDataTitle}
+      />
+    </div>
+  );
+};
+
+export default AdminTableTemplete;
