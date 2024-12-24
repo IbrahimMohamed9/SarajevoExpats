87848d8f321b5ca631e0824643c08a0389cf5fd2
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-12-24 03:22:14 +0100

    make all backend routes start by api

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/events/", require("./routes/eventRoutes"));
 app.use("/api/qaas/", require("./routes/QaARoutes"));
 app.use("/api/news/", require("./routes/newsRoutes"));
@@ -41,7 +41,7 @@ app.use("/api/services/", require("./routes/serviceRoutes"));
 app.use("/api/serviceTypes/", require("./routes/serviceTypeRoutes"));
 app.use("/api/serviceSubtypes/", require("./routes/serviceSubtypeRoutes"));
 app.use("/api/users/", require("./routes/userRoutes"));
-app.use("/photos", express.static(path.join(__dirname, "photos")));
+app.use("/api/photos", express.static(path.join(__dirname, "photos")));
 app.use("/api/upload", require("./routes/uploadRoutes"));
 app.use(errorHandler);
 
