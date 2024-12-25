ebd0d2b4ede94843fc9a3bd81f48cb379b2601b9
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-12-25 14:20:04 +0100

    t

--- client/src/components/atoms/AdSense.jsx
+++ client/src/components/atoms/AdSense.jsx
@@ -1,12 +1,18 @@
 "use client";
 
+import Script from "next/script";
+
 const AdSense = () => {
   return (
-    <script
-      async
+    <Script
+      id="adsbygoogle-script"
+      strategy="afterInteractive"
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4465300068442635"
       crossOrigin="anonymous"
-    ></script>
+      onError={(e) => {
+        console.error('AdSense script failed to load:', e);
+      }}
+    />
   );
 };
 
