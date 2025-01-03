22bb11a6a293e9d1e05ab8243631a662f96eb03a
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-01-03 14:02:25 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -7,26 +7,6 @@ const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 const path = require("path");
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://www.sarajevoexpats.com",
-      "https://sarajevoexpats.com",
-      "http://localhost:3030",
-    ];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  exposedHeaders: ["Content-Range", "X-Content-Range"],
-  maxAge: 86400,
-};
-
 const options = {
   failOnErrors: true,
   definition: {
@@ -49,8 +29,7 @@ dotenv.config();
 connectDb();
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
+
 // Configure CORS
 app.use(
   cors({
@@ -61,8 +40,7 @@ app.use(
     maxAge: 600,
   })
 );
-// Add pre-flight OPTIONS handling
-app.options("*", cors(corsOptions));
+
 app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/events/", require("./routes/eventRoutes"));
 app.use("/api/qaas/", require("./routes/QaARoutes"));
@@ -72,11 +50,7 @@ app.use("/api/placeTypes/", require("./routes/placeTypeRoutes"));
 app.use("/api/services/", require("./routes/serviceRoutes"));
 app.use("/api/serviceTypes/", require("./routes/serviceTypeRoutes"));
 app.use("/api/serviceSubtypes/", require("./routes/serviceSubtypeRoutes"));
-app.get("/api/users", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://www.sarajevoexpats.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.json({ message: "Users API is working!" });
-});
+app.use("/api/users/", require("./routes/userRoutes"));
 app.use("/api/upload", require("./routes/uploadRoutes"));
 
 const photosDir = path.join(__dirname, "photos");
