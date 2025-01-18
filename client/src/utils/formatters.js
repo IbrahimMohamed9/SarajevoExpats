890a4bc4483017b82ebd9ca0326d6ddb8c39ebf7
Ibrahim Mohamed <ibrahemmohamedb@gmail.com>
2025-01-18 15:40:03 +0100

    fix

--- client/src/utils/formatters.js
+++ client/src/utils/formatters.js
@@ -1,10 +1,12 @@
 export const formatDateTime = (dateString) => {
+  // Create a stable date object using UTC
   const date = new Date(dateString);
-  return date.toLocaleString("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'UTC'
+  }).format(date);
 };
