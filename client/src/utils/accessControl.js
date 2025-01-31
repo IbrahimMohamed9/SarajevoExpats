7e4468a1f6052b6258505000702ce6d90341c3f7
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-01-31 11:13:50 +0100

    fix token error

--- client/src/utils/accessControl.js
+++ client/src/utils/accessControl.js
@@ -1,16 +1,18 @@
 import { cookies } from "next/headers";
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = process.env.JWT_SECRET;
-
 export const verifyAdmin = () => {
   const token = cookies().get("access_token")?.value;
+
   if (!token) return false;
 
   try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    return decoded?.user?.type === "admin";
+    const decoded = jwt.decode(token);
+    const user = decoded.user;
+
+    return user.type === "admin";
   } catch (error) {
+    console.log(error);
     return false;
   }
 };
