84d3c28fa4a685c2226a6c191bc7243d263baf65
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-04 14:54:53 +0100

    handle one news in slides

--- client/src/components/organisms/NewsCarousel.jsx
+++ client/src/components/organisms/NewsCarousel.jsx
@@ -5,6 +5,12 @@ import CarouselWithSingleSlide from "@molecules/CarouselWithSingleSlide";
 import { SwiperSlide } from "swiper/react";
 
 const NewsCarousel = ({ slides }) => {
+  if (slides.length === 0) {
+    return null;
+  } else if (slides.length === 1) {
+    return <SingleNewsLayout latestNews={slides[0]} />;
+  }
+
   const slidesElements = slides.map((slide, index) => (
     <SwiperSlide key={index}>
       <SingleNewsLayout latestNews={slide} />
