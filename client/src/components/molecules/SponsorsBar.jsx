0c4df65f13025ec85884b41f49be58dd1cb54916
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-05 12:45:04 +0100

    improve sponsor bar style

--- client/src/components/molecules/SponsorsBar.jsx
+++ client/src/components/molecules/SponsorsBar.jsx
@@ -7,26 +7,22 @@ const SponsorsBar = ({ sponsors }) => {
   if (!sponsors?.length) return null;
 
   return (
-    <>
+    <div className="bg-[#ff7003]/10 py-8 px-4">
       <SectionHeder title="Trusted By" />
-      <div className="flex flex-wrap justify-center items-center gap-8 bg-[#ff7003]/10">
+      <div className="flex flex-wrap justify-center items-center gap-8">
         {sponsors.map((sponsor) => (
-          <div
+          <Image
+            src={sponsor.picture}
             key={sponsor._id}
-            className="flex items-center justify-center w-full h-32 px-4 hover:scale-105 transition-transform duration-300"
-          >
-            <Image
-              src={sponsor.picture}
-              alt={sponsor.name}
-              width={240}
-              height={120}
-              className="object-contain max-h-28"
-              title={sponsor.name}
-            />
-          </div>
+            alt={sponsor.name}
+            width={240}
+            height={120}
+            className="object-contain max-h-28 hover:scale-105 transition-transform duration-300"
+            title={sponsor.name}
+          />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
