6318e7ce7624b35ff451fb1d50ca4cd080f3e28b
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-05 12:39:06 +0100

    add section title

--- client/src/components/molecules/SponsorsBar.jsx
+++ client/src/components/molecules/SponsorsBar.jsx
@@ -7,23 +7,26 @@ const SponsorsBar = ({ sponsors }) => {
   if (!sponsors?.length) return null;
 
   return (
-    <div className="flex flex-wrap justify-center items-center gap-8 bg-[#ff7003]/10">
-      {sponsors.map((sponsor) => (
-        <div
-          key={sponsor._id}
-          className="flex items-center justify-center w-full h-32 px-4 hover:scale-105 transition-transform duration-300"
-        >
-          <Image
-            src={sponsor.picture}
-            alt={sponsor.name}
-            width={240}
-            height={120}
-            className="object-contain max-h-28"
-            title={sponsor.name}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <SectionHeder title="Truested By" />
+      <div className="flex flex-wrap justify-center items-center gap-8 bg-[#ff7003]/10">
+        {sponsors.map((sponsor) => (
+          <div
+            key={sponsor._id}
+            className="flex items-center justify-center w-full h-32 px-4 hover:scale-105 transition-transform duration-300"
+          >
+            <Image
+              src={sponsor.picture}
+              alt={sponsor.name}
+              width={240}
+              height={120}
+              className="object-contain max-h-28"
+              title={sponsor.name}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
