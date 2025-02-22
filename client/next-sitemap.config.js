f9adc813fed271e22860f7c9fd8ff5f53b2977b6
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-22 21:28:36 +0100

    feat: add additional sitemaps to robots.txt configuration

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://sarajevoexpats.com", 
-  generateRobotsTxt: true, 
-  sitemapSize: 50000, 
+  siteUrl: "https://sarajevoexpats.com",
+  generateRobotsTxt: true,
+  sitemapSize: 50000,
   exclude: ["/dashboard/*", "/api/*", "/login", "/register"],
   robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.SITE_URL}/sitemap-static.xml`,
+      `${process.env.SITE_URL}/sitemap-events.xml`,
+      `${process.env.SITE_URL}/sitemap-news.xml`,
+      `${process.env.SITE_URL}/sitemap-places.xml`,
+      `${process.env.SITE_URL}/sitemap-services.xml`,
+    ],
     policies: [
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/dashboard/", "/api/", "/login", "/register"], 
+        disallow: ["/dashboard/", "/api/", "/login", "/register"],
       },
     ],
   },
